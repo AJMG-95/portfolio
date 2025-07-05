eb5f16527a7acaed9212443bdff461affd71b817
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2025-07-05 18:02:03 +0200

    home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TranslocoModule } from '@jsverse/transloco';
+import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 import { ProfilePhoto } from '../../shared/components/profile-photo/profile-photo.component';
 import { Typewriter } from '../../shared/components/typewriter/typewriter.component';
@@ -17,19 +17,35 @@ import { Typewriter } from '../../shared/components/typewriter/typewriter.compon
     Typewriter
   ],
 })
-export class HomePage implements AfterViewInit {
+export class HomePage implements OnInit, AfterViewInit {
   @ViewChild('heading') heading!: ElementRef<HTMLElement>;
   typingDone = false;
   typing1Done = false;
   typing2Started = false;
 
+  greeting = '';
+  description = '';
+  stack = '';
+  claim = '';
+  projectsBtn = '';
+  contactBtn = '';
+
+  constructor(private transloco: TranslocoService) { }
+
+  ngOnInit(): void {
+    this.greeting = this.transloco.translate('home.greeting');
+    this.description = this.transloco.translate('home.description');
+    this.stack = this.transloco.translate('home.stack');
+    this.claim = this.transloco.translate('home.claim');
+    this.projectsBtn = this.transloco.translate('home.projectsBtn');
+    this.contactBtn = this.transloco.translate('home.contactBtn');
+  }
+
   ngAfterViewInit(): void {
     setTimeout(() => {
       this.typingDone = true;
-    }, 3000); // ⚠️ asegúrate de que coincide exactamente con 'typing 3s'
+    }, 3000);
   }
-
-
 
   onFirstTypingFinish(): void {
     this.typing1Done = true;
