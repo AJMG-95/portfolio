be23a8de3d4cfdb01725864c09589144594bbcf2
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2025-10-03 11:30:39 +0200

    style

--- src/app/portfolio/pages/about-page/about-tabs/about-tabs.component.ts
+++ src/app/portfolio/pages/about-page/about-tabs/about-tabs.component.ts
@@ -5,6 +5,7 @@ import { TranslocoPipe } from '@jsverse/transloco';
   selector: 'about-tabs',
   imports: [TranslocoPipe],
   templateUrl: './about-tabs.component.html',
+  styleUrl: './about-tabs.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutTabsComponent {
@@ -31,5 +32,35 @@ export class AboutTabsComponent {
       this.#_activeIndex.set(i);
       this.activeIndexChange.emit(i);
     }
+  }
+
+  hoverColorClasses = [
+    'hover:bg-primary/10 hover:border-primary ',
+    'hover:bg-info/10 hover:border-info',
+    'hover:bg-accent/10 hover:border-accent',
+  ];
+
+  activeColorClasses = [
+    'shadow-md shadow-primary/10 border-primary',
+    'shadow-md shadow-info/10  border-info',
+    'shadow-md shadow-accent/10  border-accent',
+  ];
+
+  // Base común a todos los tabs (sin colores)
+  private readonly baseTab =
+    'rounded-xl px-3 py-2 transition duration-300 shadow-md ' +
+    'active:animate-bounce-scale active:scale-85 ' +
+    'hover:border-t-0 hover:border-l-0 hover:border-r hover:border-b';
+
+  // Devuelve todas las clases para el tab i (sin usar ngClass)
+  tabClasses(i: number): string {
+    const base = this.baseTab;
+    const hover = this.hoverColorClasses[i] ?? '';
+    const isActive = this.activeIndex() === i;
+
+    const activeCommon = isActive ? ' border-t border-l font-semibold scale-105' : '';
+    const activeColor = isActive ? (this.activeColorClasses[i] ?? '') : '';
+
+    return [base, hover, activeCommon, activeColor].join(' ').trim();
   }
  }
