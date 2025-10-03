30a3710405ba775fcd8b09f6fa3bbfdee219a4ee
AJMG-95 <antoniojesus.marchena@iesdonana.org>
2025-10-03 10:53:16 +0200

    404

--- src/app/portfolio/pages/not-found-page/not-found-page.component.ts
+++ src/app/portfolio/pages/not-found-page/not-found-page.component.ts
@@ -1,8 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [],
+  imports: [TranslocoPipe, RouterLink],
   templateUrl: './not-found-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
